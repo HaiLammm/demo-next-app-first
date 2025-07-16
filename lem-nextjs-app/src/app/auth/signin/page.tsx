c1fcf8954608e20cd4665593e3f953d90a037b2c
HaiLammm <luonghaimal@gmail.com>
2025-07-17 05:12:01 +0700

    test the Link

---
+++ lem-nextjs-app/src/app/auth/signin/page.tsx
@@ -0,0 +1,8 @@
+const Singin = () => {
+    return (
+        <div>
+            <h1>Day la trang dang nhap</h1>
+        </div>
+    );
+};
+export default Singin;
---
+++ lem-nextjs-app/src/app/auth/singup/page.tsx
@@ -0,0 +1,8 @@
+const Singup = () => {
+    return (
+        <div>
+            <h1>Day la trang dang ki</h1>
+        </div>
+    );
+};
+export default Singup;
