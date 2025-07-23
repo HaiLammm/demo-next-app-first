0452f388683e926c04fac7e520f80a08f1fd93a4
HaiLammm <luonghaimal@gmail.com>
2025-07-23 11:59:04 +0700

    Add toggle mode

--- lem-nextjs-app/src/app/auth/singup/page.tsx
+++ lem-nextjs-app/src/app/auth/singup/page.tsx
@@ -1,6 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 const Singup = () => {
     return (
         <div>
+
+            <ModeToggle/>
             <h1>Day la trang dang ki</h1>
         </div>
     );
--- lem-nextjs-app/src/app/layout.tsx
+++ lem-nextjs-app/src/app/layout.tsx
@@ -3,42 +3,54 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Children } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <header><link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"/></header>
-                    <body
-                        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-                    >
-                        <div>Hai Lam</div>
-                        {children}
-                        <footer
-                            style={
-                                {
-                                    textAlign: "center",
-                                    padding: "10px",
-                                    backgroundColor: "#f8f8f8",
-                                } as React.CSSProperties
-                            }
-                        >
-                        </footer>
-                    </body>
-                </html>
-                );
+  return (
+    <html lang="en">
+      <div>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </div>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <footer
+          style={
+            {
+              textAlign: "center",
+              padding: "10px",
+              backgroundColor: "#f8f8f8",
+            } as React.CSSProperties
+          }
+        ></footer>
+      </body>
+    </html>
+  );
 }
